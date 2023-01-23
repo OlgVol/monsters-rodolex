cbb15b1c48c3c04b4fb40bb0c9dfc9d44cf91ac1
Olga Volkova <deviatkovaolga@gmail.com>
2023-01-23 14:29:12 +0200

    mosters-rodolex app

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { Component } from "react";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+    console.log("constroctur");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+      <h1 className="app-title">Monsters Rodolex</h1>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="Serch Monsters"
+          className="monsters-search-box"
+        />
+
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
